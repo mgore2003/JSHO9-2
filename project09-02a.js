2353921b702cedc9b4837b3637839b8aa849e38a
mgore2003 <142228045+mgore2003@users.noreply.github.com>
2024-04-29 08:52:31 -0400

    Add files via upload

---
+++ project09-02a.js
@@ -0,0 +1,35 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Project 09-02
+
+      Project to read field values from web storage
+      Author: 
+      Date:   
+
+      Filename: project09-02a.js
+*/
+
+/* Page Objects */
+
+let riderName = document.getElementById("riderName");
+let ageGroup = document.getElementById("ageGroup");
+let bikeOption = document.getElementById("bikeOption");
+let routeOption = document.getElementById("routeOption");
+let accOption = document.getElementById("accOption");
+let region = document.getElementById("region");
+let miles = document.getElementById("miles");
+let comments = document.getElementById("comments");
+
+document.getElementById("submitButton").onclick = showData;
+
+function showData() {
+	sessionStorage.setItem(riderName);
+	sessionStorage.setItem(ageGroup);
+	sessionStorage.setItem(bikeOption);
+	sessionStorage.setItem(routeOption);
+	sessionStorage.setItem(accOption);
+	sessionStorage.setItem(region);
+	sessionStorage.setItem(miles);
+	sessionStorage.setItem(comments);
+}
